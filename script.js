33605f6c0d73fa73257b6a7db4c8d4ee890c89a8
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-25 20:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 const textArea = document.getElementById('evaluatedText');
 textArea.addEventListener('input', () => {
     var textLn =  textArea.value;
-    document.getElementById('wordCount').innerHTML= getWordCount(textLn);
+    document.getElementById('wordCount').innerHTML='Total words: '+getWordCount(textLn);
 });
 
 function getWordCount(str) {
-     return str.trim().split(/\s/g).length;
+     return str.trim().split(/\s+/).length;
 }
 
